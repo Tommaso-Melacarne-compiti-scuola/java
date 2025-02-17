ef4ec1ef68fc5522abdcab761d06e49ab4b941e0
PolpOnline <git@t0mmy.anonaddy.com>
2025-02-17 08:49:14 +0100

    Continue work with battleship

--- src/battleshipFx/src/main/java/polp/online/PrimaryController.java
+++ src/battleshipFx/src/main/java/polp/online/PrimaryController.java
@@ -1,14 +1,47 @@
 package polp.online;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
 import polp.online.model.BattleshipModel;
-import polp.online.model.Board;
 import polp.online.model.Player;
 
 public class PrimaryController {
+    @FXML
+    private StackPane mainStackPane;
+
+    @FXML
     private void initialize() {
         BattleshipModel.addPlayer(new Player("Player 1"));
         BattleshipModel.addPlayer(new Player("Player 2"));
+
+        GridPane board = createBoard();
+
+        mainStackPane.getChildren().add(board);
+
+    }
+
+    private GridPane createBoard() {
+        GridPane board = new GridPane();
+        board.setPrefSize(500, 500);
+        board.setGridLinesVisible(true);
+
+        board.getColumnConstraints().add(new ColumnConstraints(50));
+        board.getRowConstraints().add(new RowConstraints(50));
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                Button button = new Button();
+                button.setPrefSize(50, 50);
+                button.getStyleClass().add("board-button");
+
+                board.add(button, i, j);
+            }
+        }
+
+        return board;
     }
 }
--- src/battleshipFx/src/main/java/polp/online/model/Player.java
+++ src/battleshipFx/src/main/java/polp/online/model/Player.java
@@ -8,4 +8,14 @@ import lombok.RequiredArgsConstructor;
 public class Player {
     private final String name;
     private final Board board = new Board();
+
+    /**
+     * Attacks a point on this player's board
+     *
+     * @param point The point to attack
+     * @return The result of the attack
+     */
+    public AttackResult attack(Point point) {
+        return board.attack(point);
+    }
 }

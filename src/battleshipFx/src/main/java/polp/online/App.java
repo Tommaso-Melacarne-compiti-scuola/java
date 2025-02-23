3b894021a8934cca6f80dcd26dd0995e555347d7
PolpOnline <git@t0mmy.anonaddy.com>
2025-02-23 19:17:52 +0100

    Add css

--- src/battleshipFx/src/main/java/polp/online/App.java
+++ src/battleshipFx/src/main/java/polp/online/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * JavaFX App
@@ -17,7 +18,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 540, 600);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
         stage.setScene(scene);
         stage.show();
     }

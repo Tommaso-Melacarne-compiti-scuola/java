e3d7c275a99781298e151837a49f828b45f0a7e0
PolpOnline <git@t0mmy.anonaddy.com>
2025-03-28 09:12:43 +0100

    Fmt random singleton

--- src/ticketsSynchronization/src/main/java/polp/online/singletons/RandomSingleton.java
+++ src/ticketsSynchronization/src/main/java/polp/online/singletons/RandomSingleton.java
@@ -7,8 +7,6 @@ import java.util.Random;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RandomSingleton {
-
-
     private static final class InstanceHolder {
         private static final Random INSTANCE = new Random();
     }
